ec4688e6f9884e70808dcf40722275b957a99317
PMT <86708711+Thang10102000@users.noreply.github.com>
2022-10-11 10:41:04 +0700

    ThangPM2

--- HTQLDL_source/src/main/java/com/neo/vas/service/AgencyOrderRequestService.java
+++
@@ -1,9 +0,0 @@
-package com.neo.vas.service;
-
-import com.neo.vas.domain.ServiceRequest;
-
-import java.util.List;
-
-public interface AgencyOrderRequestService {
-    List<ServiceRequest> getServiceRequestByAoId(Long id);
-}
